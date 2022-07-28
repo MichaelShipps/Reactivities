2c054e4593f04365f4812067b3ea3a3ebef201d1
MichaelShipps <Kobushii@gmail.com>
2022-07-27 18:27:10 -0700

    End of Section 20

--- client-app/src/app/models/profile.ts
+++ client-app/src/app/models/profile.ts
@@ -5,13 +5,13 @@ export interface Profile {
     displayName: string;
     image?: string;
     bio?: string;
+    followersCount: number;
+    followingCount: number;
+    following: boolean;
     photos?: Photo[];
 }
 
 export class Profile implements Profile {
-    /**
-     *
-     */
     constructor(user: User) {
         this.username = user.username;
         this.displayName = user.displayName;
