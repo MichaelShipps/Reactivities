c8854fc28b5fcdba9bc4bd2f85be83005e121393
MichaelShipps <Kobushii@gmail.com>
2022-10-21 16:51:24 -0700

    FB login worked finish?

--- API/Startup.cs
+++ API/Startup.cs
@@ -71,14 +71,15 @@ namespace API
                 ))
                 .FontSources(s => s.Self().CustomSources(
                     "https://fonts.gstatic.com",
-                    "data:",
+                    "data:;",
                     "https://cdn.jsdelivr.net/npm/semantic-ui@2/"
                 ))
                 .FormActions(s => s.Self())
                 .FrameAncestors(s => s.Self())
                 .ImageSources(s => s.Self().CustomSources(
                     "https://res.cloudinary.com",
-                    "blob:",
+                    "blob:;",
+                    "data:;",
                     "https://www/facebook.com"
                 ))
                 .ScriptSources(s => s.Self()
